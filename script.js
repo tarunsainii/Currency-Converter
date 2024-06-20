e1842f3dddf6e594fd27296a95f9230b41ee854e
Tarun Saini <158452876+tarunsainii@users.noreply.github.com>
2024-06-20 15:35:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+const BASE_URL = "https://currency-api.pages.dev/v1/currencies/";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+
+for (let select of dropdowns) {
+  for (currCode in countryList) {
+    let newOption = document.createElement("option");
+    newOption.innerText = currCode;
+    newOption.value = currCode;
+    if (select.name === "from" && currCode === "USD") {
+      newOption.selected = "selected";
+    } else if (select.name === "to" && currCode === "INR") {
+      newOption.selected = "selected";
+    }
+    select.append(newOption);
+  }
+
+  select.addEventListener("change", (evt) => {
+    updateFlag(evt.target);
+  });
+}
+
+const updateFlag = (element) => {
+  let currCode = element.value;
+  let countryCode = countryList[currCode];
+  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  let img = element.parentElement.querySelector("img");
+  img.src = newSrc;
+};
+
+btn.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  updateExchangeRate();
+});
+
+const updateExchangeRate = async () => {
+  let amount = document.querySelector(".amount input");
+  let amtValue = amount.value;
+  if (amtValue === "" || amtValue < 1) {
+    amtValue = 1;
+    amount.value = "1";
+  }
+
+  const URL = `${BASE_URL}${fromCurr.value.toLowerCase()}.json`;
+  let response = await fetch(URL);
+  let data = await response.json();
+  let dataa = data[fromCurr.value.toLowerCase()];
+  let rate = dataa[toCurr.value.toLowerCase()];
+
+  let finalAmount = amtValue * rate;
+  msg.innerText = `${amtValue} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+};
+
+window.addEventListener("load", () => {
+  updateExchangeRate();
+});
